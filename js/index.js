a8663899f09f4000b2fd8ea3470f77fe001e04d4
DzmitryGrom <Dzmitry_Shaliaheika@epam.com>
2020-06-29 09:46:02 +0300

    added game

---
+++ js/index.js
@@ -0,0 +1,85 @@
+(function () {
+    const canvas = document.getElementById('game');
+    const ctx = canvas.getContext("2d");
+    const bg = new Image();
+    const foodImg = new Image();
+    let box = 32;
+    let score = 0;
+    let food = {
+        x: Math.floor((Math.random() * 17 + 1)) * box,
+        y: Math.floor((Math.random() * 15 + 3)) * box,
+    };
+    let snake = [];
+    let dir
+
+    snake[0] = {
+        x: 9 * box,
+        y: 10 * box
+    };
+
+    document.addEventListener('keydown', direction);
+
+    function direction(e) {
+        if (e.keyCode === 37 && dir !== 'right') dir = 'left';
+        
+        if (e.keyCode === 38 && dir !== 'up')  dir = 'down';
+        
+        if (e.keyCode === 39 && dir !== 'left') dir = 'right';
+        
+        if (e.keyCode === 40 && dir !== 'down') dir = 'up';
+    }
+
+    bg.src = "img/bg.png";
+    foodImg.src = "img/food.png";
+
+    function eatTail(head, arr) {
+        for (let i = 0; i < arr.length; i++){
+            if(head.x === arr[i].x && head.y === arr[i].y) clearInterval(game)
+        }  
+    };
+
+    function drawGame() {
+        ctx.drawImage(bg, 0, 0);
+        ctx.drawImage(foodImg, food.x, food.y);
+        for (let i = 0; i < snake.length; i++) {
+            ctx.fillStyle = i === 0 ? 'red' : 'black';
+            ctx.fillRect(snake[i].x, snake[i].y, box, box);
+        }
+        ctx.fillStyle = 'white';
+        ctx.font = '50px Arial';
+        ctx.fillText(score, box * 3, box * 1.7);
+
+        let snakeX = snake[0].x;
+        let snakeY = snake[0].y;
+
+        if (snakeX === food.x && snakeY === food.y) {
+            score++;
+            food = {
+                x: Math.floor((Math.random() * 17 + 1)) * box,
+                y: Math.floor((Math.random() * 15 + 3)) * box,
+            };
+        } else {
+            snake.pop();
+        }
+
+        if (snakeX < box || snakeX > box*17 || snakeY < 3*box || snakeY > box* 17) {
+            clearInterval(game);
+        }
+
+        if (dir === 'left') snakeX -= box;
+        if (dir === 'right') snakeX += box;
+        if (dir === 'up') snakeY += box;
+        if (dir === 'down') snakeY -= box;
+
+        let newHead = {
+            x: snakeX,
+            y: snakeY
+        }
+
+        eatTail(newHead, snake);
+        snake.unshift(newHead);
+    }
+
+    let game = setInterval(drawGame, 100)
+
+})()
